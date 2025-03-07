15ddb91ee400fb9e51dd205033c727d0c94a1a8f
TANI1864 <taniabeltran2020@gmail.com>
2025-03-06 19:29:49 -0500

    DadoDelDestino

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Random;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class DadoDelDestino {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int vidas = 5;
+        int puntos = 0;
+
+        while (vidas > 0 && puntos <= 10) {
+            System.out.println("Presiona Enter para lanzar el dado");
+            scanner.nextLine();
+
+            int dado = random.nextInt(6) + 1;
+            System.out.println("Tu dado ha caido en: " + dado);
+
+            if (dado == 1) {
+                vidas--;
+                System.out.println("Haz perdido una vida. Tus vidas restantes son: " + vidas);
+            } else if (dado == 6) {
+                puntos += 3;
+                System.out.println("¡Haz ganado 3 puntos!Tus puntos totales son: " + puntos);
+            } else if (dado == 2 || dado == 4) {
+                puntos += 1;
+                System.out.println("¡Haz ganado 1 punto!Tus puntos totales son: " + puntos);
+            } else {
+                System.out.println("No pasó nada.Tus puntos totales son: " + puntos);
+            }
         }
+
+
+        if (vidas == 0) {
+            System.out.println("¡Te quedaste sin vidas! El juego Finalizo.");
+        } else {
+            System.out.println("¡Has alcanzado más de 10 puntos! ¡Ganaste!");
+        }
+
     }
 }
